a7594b72d5b6e86c9c876a5d4787c8514611537b
G0AT <miguel.gonzalezl@hotmail.com>
2022-12-07 01:59:54 -0600

    Carga de tailwind en componentes

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,18 @@
 const Formulario = () => {
     return (
-        <div>
-            <h2>Formulario</h2>
+        <div className="md:w-1/2 lg:w-2/5 mx-5">
+            <h2 className="font-black text-3xl text-center">Seguimiento pacientes</h2>
+
+            <p className="text-lg mt-5 text-center mb-10">Añade pacientes y {''}
+                <span className="text-indigo-600 font-bold text-lg">Administralos</span>
+            </p>
+
+            <form className="bg-white shadow-md rounded-lg py-10 px-5">
+                <div className="block text-gray-700 uppercase font-bold">
+                    <label htmlFor="">Nombre Mascota</label>
+                    <input type="text" placeholder="Nombre de la mascota" className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md" name="" id="" />
+                </div>
+            </form>
         </div>
     )
 }
