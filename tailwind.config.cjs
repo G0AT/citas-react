9e9fa583603f202b22dfd319f0ae8838bcf7ddce
G0AT <miguel.gonzalezl@hotmail.com>
2022-12-08 23:29:28 -0600

    Finalizado, sin base de datos, usando local storage, todo se borra al actualizar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./index.html", "./src/*/**/*.jsx" ],
+  //content: [ "./index.html", "./src/*/**/*.jsx" ], //poner cuando este en desarrollo
+  content: [ "index.html", "./src/*/**/*.jsx" ], //poner cuando se haga el build
   theme: {
     extend: {},
   },
